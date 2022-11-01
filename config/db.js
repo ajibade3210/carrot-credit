b3dc482bde6d78574fe7ee955bee994bdb554239
ajibade3210 <ajibade69th@gmail.com>
2022-11-01 09:15:46 +0100

    make it better

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 
-db.connect(function (err) {
   db.query(
     `create table if not exists Client ( id int auto_increment primary key, firstname VARCHAR(255) not null, lastname VARCHAR(255) not null, amount INT not null, accountType VARCHAR(255), createdAt DATE not null)`,
     function (err, result) {
@@ -16,8 +18,6 @@ db.connect(function (err) {
       console.log("Client table created");
     }
   );
-db.end();
   console.log("Database Connected!");
-});
 
 module.exports = db.promise();
